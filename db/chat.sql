b8ef465e0a65f9e4341d9465460c39dc76014e2a
hasan <abdul.hasan388@gmail.com>
2021-07-10 00:31:51 +0700

    screenshoot dan database

---
+++ db/chat.sql
@@ -0,0 +1,107 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 09, 2021 at 07:26 PM
+-- Server version: 10.1.26-MariaDB
+-- PHP Version: 7.1.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `chat`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pesan`
+--
+
+CREATE TABLE `pesan` (
+  `id_pesan` int(11) NOT NULL,
+  `id_user` int(15) NOT NULL,
+  `id_lawan` int(15) NOT NULL,
+  `isi` text NOT NULL,
+  `waktu` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `pesan`
+--
+
+INSERT INTO `pesan` (`id_pesan`, `id_user`, `id_lawan`, `isi`, `waktu`) VALUES
+(95, 2, 1, 'halo\n', '2021-07-10 00:00:22'),
+(96, 1, 2, 'apa sewta?', '2021-07-10 00:00:40'),
+(97, 2, 1, 'oke', '2021-07-10 00:00:50'),
+(98, 1, 3, 'oy', '2021-07-10 00:01:05'),
+(99, 3, 1, 'appAN', '2021-07-10 00:01:17'),
+(100, 1, 3, 'GG LU', '2021-07-10 00:01:25');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id_user` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `nama` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id_user`, `username`, `nama`) VALUES
+(1, 'abdul', 'abdul'),
+(2, 'hasan', 'hasan'),
+(3, 'bedul', 'bedul');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `pesan`
+--
+ALTER TABLE `pesan`
+  ADD PRIMARY KEY (`id_pesan`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id_user`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `pesan`
+--
+ALTER TABLE `pesan`
+  MODIFY `id_pesan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=101;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ readme.rst
@@ -0,0 +1,16 @@
+ini aplikasi chat sederhana
+
+menggunakan :
+- php framework Codeigniter 3
+- jquery ajax
+- sweetalert
+- database mysql
+
+
+silahkan download dengan free, 
+dan sangat di perbolehkan di edit atau di gunakan untuk pembelajaran
+apabila mau di share ke web atau youtube di perbolehkan, asal dengan sumber git saya atau web saya 
+https://heyiamhasan.com
+free donasi dengan wa 089602340857 
+atau jika mau modifikasi wa saja
+screenshot ada di folder shreenshoo
